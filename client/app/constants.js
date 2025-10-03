1df498d1441cc4e0f7d8687c2c328292be351a31
Din Saadon <din.saadon1@gmail.com>
2025-10-03 05:31:56 +0000

    Edit constants.js
    move backend urls to env vars, prod default empty

--- client/app/constants.js
+++ client/app/constants.js
@@ -1,11 +1,4 @@
 // constants.js
 const IS_LOCAL = process.env.NODE_ENV === "development";
 
-export const BACKEND_URL = IS_LOCAL
-  ? "http://localhost:8000"      // local backend
-  : "https://backend-igem.onrender.com";
- // production backend
-
-export const EMAIL_BACKEND_URL = IS_LOCAL
-  ? "http://localhost:8000"     // local email backend
-  : "https://backend-igem.onrender.com"; 
+export const BACKEND_URL = process.env.BACKEND_URL || (IS_LOCAL ? "http://localhost:8000" : "");
